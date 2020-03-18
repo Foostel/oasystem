f48be2ae5c77d9994de4743273a530532cf5b71c
Mihir Soni <msonii2000@gmail.com>
2020-03-18 14:45:44 +0530

    form validation done

--- actions.js
+++ actions.js
@@ -58,3 +58,98 @@ function revoke(s){
         h2=true;
     }
 }
+function passC(){
+    var p = document.getElementById('pass1').value;
+    var regex = new Array();
+    regex.push(/[A-Z]/); //Uppercase Alphabet.
+    regex.push(/[a-z]/); //Lowercase Alphabet.
+    regex.push(/[0-9]/); //Digit.
+    regex.push(/[@$!%*#?&]/); //Special Character.
+    let count=0;
+    for(let i=0;i<4;i++)
+    {
+        if(regex[i].test(p)){
+            count++;
+        }
+    }
+    if(p=""){
+        count=0;
+    }
+    switch(count){
+        case 0:
+        console.log("case0");
+        document.getElementById('pass1').style.color="black";
+        break;
+        case 1:
+        console.log("case1");
+        document.getElementById('pass1').style.color="red";
+        break;
+        case 2:
+        console.log("case2");
+        document.getElementById('pass1').style.color="orange";
+        break;
+        case 3:
+        console.log("case3");
+        document.getElementById('pass1').style.color="green";
+        break;
+        case 4:
+        console.log("case4");
+        document.getElementById('pass1').style.color="green";
+        break;       
+    }
+
+    pst=count;
+    console.log(pst);
+    return pst;
+}
+function validate(){
+    let pst=0;
+    let f=true;
+    console.log("call");
+    alert("validate?");
+    let x = document.getElementById("phoneno").value;
+    let p1 = document.getElementById("pass1").value;
+    let p2 = document.getElementById("pass2").value;
+    
+    var rx = /^[0-9]{10}$/;
+    if(!rx.test(x)){
+        f=false;
+        alert("Please enter valid contact number");
+    }
+    pst= passC();
+    console.log(pst);
+    if(!(pst>2) && !(p1.length>7))
+    {
+        alert("Created password is too weak");
+        f=false;
+    }
+    else{
+        if(p1!=p2){
+        alert("passwords doesn't match");
+        f=false;
+    }
+    }
+    
+    if(f){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+function showp(){
+    console.log(100);
+    let x= document.getElementById('pass1');
+    if(x.type=='password'){
+    console.log(111);
+    x.type='text'; 
+    document.getElementById('eye').className='fa fa-eye';    
+    }
+    else{
+    x.type='password'; 
+    document.getElementById('eye').className='fa fa-eye-slash';   
+
+    }
+}
--- register.php
+++ register.php
@@ -4,7 +4,7 @@
     <title>Register</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script type="text/javascript" src="actions.js"></script>
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
     </script>
         <script type="text/javascript">
@@ -16,32 +16,23 @@
             });
 
         </script>
-
-        <script type="text/javascript">
-            function validate()
-            {   document.alert("hello");
-                let phoneno = document.getElementById('phoneno');
-                document.alert(phoneno);
-                return true;
-            }
-        </script>
-
+    <script src="actions.js"></script>    
 </head>
 <body style="background-color: #E9E9E9;">
     <div class="header" id="header"></div>
     <div class="section" onclick="if(!h){toggle('pop-up');}">
         <div id="register">
             <h2 style="text-align:left;">REGISTER</h2>
-            <form>
+            <form onsubmit="return validate();" method="get" action="">
                 <table cellspacing="20px" cellpadding="5px" style="border-top:solid">
                     <tr><td>First Name: </td> <td><input style="width: 200px; height: 30px; margin: 0px;" class="inputs" type="text" name="" required=""></td> <td>Last Name:</td> <td><input class="inputs" style="width: 200px; margin: 0px; height: 30px; "type="text" name=""></td></tr>
                     <tr><td>Gender: </td> <td>Male<input  type="radio" name="Gender" value="Male" required=""> Female<input type="radio" value="Female" name="Gender" required=""></td> <td>Age: </td> <td><input  class="inputs" type="number" name=" " style="width: 50px; margin: 0px; height: 30px;" required=""> </td></tr>
 
-                    <tr><td>Phone: </td> <td><input  style="width: 200px; height: 30px; margin: 0px;" class="inputs" name="phoneno" id="phoneno" required=""></td> <td>Email: </td> <td><input  class="inputs" type="Email" style="width: 200px; margin: 0px; height: 30px;"> </td></tr>
+                    <tr><td>Phone: </td> <td><input  style="width: 200px; height: 30px; margin: 0px;" type="number" class="inputs" name="phoneno" id="phoneno" required=""></td> <td>Email: </td> <td><input  class="inputs" type="Email" style="width: 200px; margin: 0px; height: 30px;"> </td></tr>
                     
                     <tr><td>Create Password: </td> <td><input  style="width: 200px; height: 30px; margin: 0px;" class="inputs" type="Password" name="" required=""></td> <td>Confirm Password: </td> <td><input  class="inputs" type="Password" style="width: 200px; margin: 0px; height: 30px;"> </td></tr>
                 </table>
-            <input type="submit" class="btn" name="" value="Submit" onsubmit="validate()">
+            <input type="submit" class="btn" name="" value="Submit">
             </form>
             <br>
             <form>
