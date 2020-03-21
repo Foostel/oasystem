b76b97841d2264c1ff07f4d8f4297e5faf292608
Jayant211998 <60085651+Jayant211998@users.noreply.github.com>
2020-03-20 17:35:52 -0700

    cycyvk

--- sign-in-verify.php
+++ sign-in-verify.php
@@ -1,15 +1,15 @@
 <?php
 include("db-connection.php");
 $email=$_POST['e-mail'];
-$password=$_POST['pass'];
+$password=md5($_POST['pass']);
+
 $query="select * from user where email='$email' and password='$password'";
-//echo $query;
-//echo mysqli_num_rows(mysqli_query($conn,$query));
 if (mysqli_num_rows(mysqli_query($conn,$query))==0){
 	echo "Please Register";}
 else{
     session_start();
-    echo "session started";
+
+   
 }
 
 ?>
