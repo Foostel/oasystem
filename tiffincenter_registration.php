2fdeab6c4bd01a13d765fc92911675f021206908
Jayant211998 <60085651+Jayant211998@users.noreply.github.com>
2020-03-21 14:49:44 -0700

    123

---
+++ tiffincenter_registration.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<script src="validation.js"></script>
+</head>
+<body>
+<hr>
+<br>
+<form action="tiffen.php" method="post" onsubmit="return validate1()">
+<lable>Building/House Number</lable>
+<input type="text" name="b" id="b"><br>
+<span id="add1" class="text-danger font-weidth-bold" style="color:red"></span>
+<br>
+<lable>Area</lable>
+<input type="text" name="a" id="a"><br>
+<span id="add2" class="text-danger font-weidth-bold" style="color:red"></span>
+<br>
+<lable>State</lable>
+<input type="text" name="s" id="s"><br>
+<span id="add3" class="text-danger font-weidth-bold" style="color:red"></span>
+<br>
+<lable>City</lable>
+<input type="text" name="c" id="c"><br>
+<span id="add4" class="text-danger font-weidth-bold" style="color:red"></span>
+<br>
+<lable>Landmark</lable>
+<input type="text" name="l" id="l"><br>
+<span id="add5" class="text-danger font-weidth-bold" style="color:red"></span>
+<br>
+<lable>Pincode</lable>
+<input type="number" name="p" id="p"><br>
+<span id="add6" class="text-danger font-weidth-bold" style="color:red"></span><br>
+
+<label>Meals Provided</label><br>
+<input type="checkbox" name="f1" id="f1" value="f1">Lunch<br>
+<input type="checkbox" name="f2" id="f2" value="f2">Dinner<br>
+<input type="checkbox" name="f3" id="f3" value="f3">Breakfast<br>
+<hr>
+<br>
+<label>Facilities Provided</label><br>
+<input type="checkbox" name="f1" id="f1" value="f1">Home Delivery<br>
+<input type="checkbox" name="f2" id="f2" value="f2">Come And Eat<br>
+<input type="checkbox" name="f3" id="f3" value="f3">Customization<br>
+<hr>
+<br>
+<table>
+<tr>
+	<th>Days</th>
+	<th>Menu</th>
+</tr>
+<tr>
+	<td>Week Days</td>
+	<td><textarea row="50" column="100" name="WeekDaysMenu" id="WeekDaysMenu"></textarea></td>
+</tr>
+<tr>
+	<td>WeekEnd Menu</td>
+	<td><textarea row="50" column="100" name="WeekDaysMenu" id="WeekDaysMenu"></textarea>  </td>
+</tr>
+</table>
+<hr>
+<table>
+<tr>
+	<th>Meal</th>
+	<th>Time</th>
+	<th>Price</th>
+	<th>
+</tr>
+<tr>
+	<td>Breakfast</td>
+	<td><input type="text" name="breakfastTime" id="breakfastTime">
+	<td><input type="text" name="breakfastFee" id="breakfastFee"><br>
+</td>
+</tr>
+<tr>
+	<td>Lunch</td>
+	<td><input type="text" name="lunchTime" id="lunchTime">
+	<td><input type="text" name="lunchFee" id="lunchFee"><br>
+</td>
+<tr>
+	<td>Dinner</td>
+	<td><input type="text" name="dinnerTime" id="dinnerTime">
+	<td><input type="text" name="dinnerFee" id="dinnerFee">
+	<br>
+</td>
+</table>
+<hr>
+Discount or Concession<br>
+<textarea row="50" column="100" name="Discount" id="Discount"></textarea>
+<hr>
+<br><input type="submit" name="submit" value="SUBMIT">
+</form>
+</body>
+</html>
--- validation.js
+++ validation.js
@@ -1,95 +1,53 @@
- function register()
+
+function validate1()
 {
-	window.location.href = "personal_details.html";
-}
-function valid()
-{
-		var name=document.getElementById("name").value.trim();
-		var email=document.getElementById("email").value.trim();
-		var contact=document.getElementById("contact").value.trim();
-		if((name.length<=2)||(name.length>=20))
-     		{
-     			document.getElementById("uname").innerHTML="**Length Of name should be between 2 to 20";
-     			return false;
-     		}
-     	if(!isNaN(name))
-     		{
-     	       document.getElementById('uname').innerHTML="** Only Characters are allowed";
-     			return false;
-     		}
-     	if(email=="")
-     		{
-     			document.getElementById("email").innerHTML="**Please Enter Email";
-     			return false;
-     		}
-     	if(email.indexOf('@',0)==0)
-     		{
-     			document.getElementById('email').innerHTML="**Invalid Email";
-     			return false;
-     		}
-     	if((email.charAt(email.length-4)!='.')&&(emails.charAt(email.length-3)!='.'))
-     		{
-     			document.getElementById('email').innerHTML="**Invalid Email";
-     			return false;
-     		}
-     	if(contact=="")
-     		{
-     			document.getElementById("phone").innerHTML="**Please Enter Contact Number";
-     			return false;
-     		}
-     	if(isNaN(contact))
-     		{
-     			document.getElementById('phone').innerHTML="**Characters are Not Allowed";
-     			return false;
-     		}
-     	if(contact.length!=10)
-     		{
-     		 document.getElementById('phone').innerHTML="**Please Enter  10 digit Mobile Number";
-     			return false;
-     		}
-}
-function validate()
-{
-	 	var uid=document.getElementById("uid").value.trim();
-		var hid=document.getElementById("hid").value.trim();
-		//var hname=document.getElementById("hname").value;
-		var address=document.getElementById("address").value.trim();
-		var area=document.getElementById("area").value.trim();
-		var pincode=document.getElementById("pincode").value.trim();
-		var landmark=document.getElementById("landmark").value.trim();	     	 
-	 	   if(uid=="")
-     		{
-     			document.getElementById("userid").innerHTML="**Please Register First";
-     			return false;
-     		}
-     		if(hid=="")
-     		{
-     			document.getElementById("hostelid").innerHTML="**Please Register First";
-     			return false;
-     		}
-     		// if(hname=="")
-     		// {
-     		// 	document.getElementById("hostelname").innerHTML="**Enter hostel name";
-     		// 	return false;
-     		// }
-     		if(address=="")
-     		{
-     			document.getElementById("add").innerHTML="**Please Enter Hostel Name";
-     			return false;
-     		}
-     		if(area=="")
-     		{
-     			document.getElementById("area1").innerHTML="**Please Enter Area";
-     			return false;
-     		}
-     		if(pincode=="")
-     		{
-     			document.getElementById("pin").innerHTML="**Please pincode";
-     			return false;
-     		}
-     		if(landmark=="")
-     		{
-     			document.getElementById("lm").innerHTML="**Please landmark";
-     			return false;
-     		}     		
+          //var uid=document.getElementById("uid").value.trim();
+          //var hid=document.getElementById("hid").value.trim();
+          //var hname=document.getElementById("hname").value;
+          var bno=document.getElementById("b").value.trim();
+          var area=document.getElementById("a").value.trim();
+          var state=document.getElementById("s").value.trim();
+          var city=document.getElementById("c").value.trim();
+          var pincode=document.getElementById("p").value.trim();
+          var landmark=document.getElementById("l").value.trim();              
+             if(bno=="")
+               {
+                    document.getElementById("add1").innerHTML="**Please Enter house or building Number";
+                    return false;
+               }
+               if(area=="")
+               {
+                    document.getElementById("add2").innerHTML="**Please Enter Property Area";
+                    return false;
+               }
+               // if(hname=="")
+               // {
+               //   document.getElementById("hostelname").innerHTML="**Enter hostel name";
+               //   return false;
+               // }
+               if(state=="")
+               {
+                    document.getElementById("add3").innerHTML="**Please Select Your State";
+                    return false;
+               }
+               if(city=="")
+               {
+                    document.getElementById("add4").innerHTML="**Please Select your City";
+                    return false;
+               }
+               if(pincode=="")
+               {
+                    document.getElementById("add5").innerHTML="**Please Enter pincode";
+                    return false;
+               }
+               if(pincode.length!=6)
+               {
+                document.getElementById("add6").innerHTML="**Please Enter Valid 6-digit pincode";
+                    return false;    
+               } 
+               if(landmark=="")
+               {
+                    document.getElementById("add7").innerHTML="**Please Enter nearby landmark";
+                    return false;
+               }              
 }
