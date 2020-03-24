98d3e60e66f15191f6bc6395764117b69851e1a4
Jayant211998 <60085651+Jayant211998@users.noreply.github.com>
2020-03-23 18:49:46 -0700

    123

--- tiffen1.php
+++ tiffen1.php
@@ -20,41 +20,41 @@ if(isset($_POST['submit']) and isset($_SESSION['id']))
  	 	$l=0;
  	$d=0;
  	$b=0;
- 	if(isset($_POST['meal']))
- 	{$meal=$_POST['meal'];
- 	 foreach($meal as $array)
-  		{
- 		  	if ($array=="Lunch" )
-   				{$l=1;}
-  			if ($array=="Dinner")
-  				{$d=1;}
-  			if ($array=="Breakfirst")
-  	 			{$b=1;}
-  		}
+ 	if(isset($_POST['breakfast']))
+ 	{$b=1; 
+  	}
+  	if(isset($_POST['lunch']))
+ 	{$b=l; 
+  	}
+  	if(isset($_POST['dinner']))
+ 	{$b=1; 
   	}	
  	//echo $lunch." ".$dinner." ".$breakfirst;  
+ 
+
  	$hd=0;
  	$cae=0;
  	$c=0;
- 	if(isset($_POST['facilities']))
- 	{$facility=$_POST['facilities'];
- 	foreach($facility as $array)
-  		{
- 		  	if ($array=="hd" )
-   				{$hd=1;}
-  			if ($array=="cae")
-  				{$cae=1;}
-  			if ($array=="c")
-  	 			{$c=1;}
-  		}
+ 	if(isset($_POST['Customization']))
+ 	{$c=1
+  	}
+  	if(isset($_POST['home_delivery']))
+ 	{$hd=1
+  	}
+  	if(isset($_POST['Come_and_eat']))
+ 	{$cae=1
   	}	
  	//echo $hd." ".$cae." ".$c;  
-  	$bt=$_POST['bt'];
+  	$bt=$_POST['bt1']." ".$_POST['bt2'];
   	$bf=$_POST['bf'];
-  	$lt=$_POST['lt'];
+  	$lt=$_POST['lt1']." ".$_POST['lt2'];
   	$lf=$_POST['lf'];
-  	$dt=$_POST['dt'];
+  	$dt=$_POST['dt1']." ".$_POST['dt2'];
   	$df=$_POST['df'];
+  	$payb=$_POST['payb'];
+  	$payl=$_POST['payl'];  	
+  	$payd=$_POST['payl'];
+
   	$query="insert into tiffin_facility(uid,l,lt,lf,b,bt,bf,d,dt,df) values($uid,$l,'$lt','$lf',$b,'$bt','$bf',$d,'$dt','$df')";
   	$q=mysqli_query($conn,$query);
   if($q){
