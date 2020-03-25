414792e6ee447733854bcb8cad7aa2980e262c85
Jayant211998 <60085651+Jayant211998@users.noreply.github.com>
2020-03-25 16:13:47 -0700

    123

--- foostel (2).sql
+++ foostel (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 21, 2020 at 11:02 PM
+-- Generation Time: Mar 26, 2020 at 12:12 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.10
 
@@ -25,75 +25,57 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tiffin_address`
+-- Table structure for table `hostel_info`
 --
 
-CREATE TABLE `tiffin_address` (
-  `uid` int(7) NOT NULL,
-  `cid` int(7) NOT NULL,
-  `bno` varchar(10) NOT NULL DEFAULT '',
-  `area` varchar(50) NOT NULL DEFAULT '',
-  `state` varchar(40) NOT NULL DEFAULT '',
-  `city` varchar(30) NOT NULL DEFAULT '',
-  `landmark` varchar(50) NOT NULL DEFAULT '',
-  `pincode` int(6) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(6) NOT NULL,
-  `fname` varchar(50) NOT NULL,
-  `lname` varchar(50) NOT NULL,
-  `gender` varchar(6) NOT NULL,
-  `age` int(3) NOT NULL,
-  `phone` varchar(15) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(35) NOT NULL
+CREATE TABLE `hostel_info` (
+  `uid` int(6) NOT NULL,
+  `cid` int(6) NOT NULL,
+  `b1` int(3) NOT NULL DEFAULT 0,
+  `ab1` int(3) NOT NULL DEFAULT 0,
+  `rb1` int(5) NOT NULL DEFAULT 0,
+  `b2` int(3) NOT NULL DEFAULT 0,
+  `ab2` int(3) NOT NULL DEFAULT 0,
+  `rb2` int(5) NOT NULL DEFAULT 0,
+  `b3` int(3) NOT NULL DEFAULT 0,
+  `ab3` int(3) NOT NULL DEFAULT 0,
+  `rb3` int(5) NOT NULL DEFAULT 0,
+  `b4` int(3) NOT NULL DEFAULT 0,
+  `ab4` int(3) NOT NULL DEFAULT 0,
+  `rb4` int(5) NOT NULL DEFAULT 0,
+  `cm` int(5) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `hostel_info`
 --
 
-INSERT INTO `user` (`id`, `fname`, `lname`, `gender`, `age`, `phone`, `email`, `password`) VALUES
-(11121, 'jayant', 'gawali', 'Male', 21, '919340216311', 'jayantgawali98@gmail.com', 'f0ec91761506810419287bb0dadec846');
+INSERT INTO `hostel_info` (`uid`, `cid`, `b1`, `ab1`, `rb1`, `b2`, `ab2`, `rb2`, `b3`, `ab3`, `rb3`, `b4`, `ab4`, `rb4`, `cm`) VALUES
+(11121, 10001, 1, 4, 7000, 1, 4, 5000, 0, 0, 0, 0, 0, 0, 5000),
+(11121, 10002, 1, 2, 3000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5000),
+(11121, 10003, 1, 2, 3000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5000),
+(11121, 10004, 1, 2, 3000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5000),
+(11121, 10005, 1, 2, 3000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5000);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tiffin_address`
+-- Indexes for table `hostel_info`
 --
-ALTER TABLE `tiffin_address`
+ALTER TABLE `hostel_info`
   ADD PRIMARY KEY (`cid`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `tiffin_address`
+-- AUTO_INCREMENT for table `hostel_info`
 --
-ALTER TABLE `tiffin_address`
-  MODIFY `cid` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10000;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11122;
+ALTER TABLE `hostel_info`
+  MODIFY `cid` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10006;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
