5b1171c840d135f588aba6dc09466cc92bcb259c
Mihir Soni <msonii2000@gmail.com>
2020-03-26 19:23:38 +0530

    c1

--- pp-upload.php
+++ pp-upload.php
@@ -24,10 +24,10 @@ if(isset($_POST['submit']))
 		$imgTypes = array('jpg','jpeg','png');
 		$add=checkExtension($filetype,$imgTypes);
 		if($add)
-		{	echo '---- '.$_FILES['photo']['tmp_name'].'---- ';
+		{
 			if(move_uploaded_file($_FILES['photo']['tmp_name'], $target_file))
 				{	$s = mysqli_query($conn,"UPDATE User SET pp = '$target_file' WHERE id = '$id';");
-					if($s){echo"sucess";}else{echo"failur";}
+					if($s){header('location:user-home.php');}else{echo'DB-FAIL';}
 				}
 				else{
 					echo"<center>Something went wrong. :(</center>";
