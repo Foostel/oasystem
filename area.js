1e3cbe2f865ba0c3a441f34abe5f85c5c386cfe8
Mihir Soni <msonii2000@gmail.com>
2020-04-05 17:24:21 +0530

    go

---
+++ area.js
@@ -0,0 +1,16 @@
+
+function getin(){
+var search_input = document.getElementById('search_input').value;
+document.getElementById('search_res_list').innerHTML = "";
+$.getJSON('https://api.mapbox.com/geocoding/v5/mapbox.places/{Indore '+search_input+'}.json?&access_token=pk.eyJ1IjoibWloaXJzb25pNzgxIiwiYSI6ImNrOGlrZTc5ajAwcnkzbHFxd3NkbnZwc3UifQ.qoBrl5wvQ6LjGZd369FnIg', function(data) {
+        for(var i=0; i<data['features'].length;i++)
+        {
+        	console.log(data['features'][i]);
+        	document.getElementById('search_res_list').innerHTML+="<li onclick='setin(this.innerHTML);'>"+data['features'][i]['place_name']+"</li>";
+        }
+        });
+}
+function setin(v){
+	document.getElementById('search_input').value=v;
+	document.getElementById('search_res_list').innerHTML="";
+}
