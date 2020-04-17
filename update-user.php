b7099d73c1cbd84a1d55a6bb78b54eebdbbf307e
Mihir Soni <msonii2000@gmail.com>
2020-04-17 20:46:57 +0530

    Update update-user.php

--- update-user.php
+++ update-user.php
@@ -6,9 +6,8 @@ if(isset($_SESSION['id']))
 $username = explode(" ",trim($_POST['username']));
 $phoneno = $_POST['phoneno'];
 $id=$_SESSION['id'];
-//echo "".$id." ";
-
-$sql='update user set fname="$username[0]",lname="$username[0]",phone="$phoneno" where id = "$id"' ;
+echo "".$id." ";
+$sql="update user set fname='$username[0]',lname='$username[1]',phone='$phoneno' where id = '$id';" ;
 if ($conn->query($sql) === TRUE) {
     echo "Record updated successfully";
 } else {
