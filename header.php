84449d6bb35036674a7f3c4d6787f37b8d48303b
Mihir Soni <msonii2000@gmail.com>
2020-04-29 17:19:01 +0530

    test

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <script type="text/javascript">
      function show(obj)
      { if(document.getElementById('menu-slide').style.display=='none')
