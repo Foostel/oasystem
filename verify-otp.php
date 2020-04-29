1d87ec5a9df2668269c1eee6d2038feec60b7a2c
Mihir Soni <msonii2000@gmail.com>
2020-04-29 18:57:11 +0530

    update

--- verify-otp.php
+++ verify-otp.php
@@ -4,6 +4,7 @@
 	<title>
 		Reset Password		
 	</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
         </script>
 	<link rel="stylesheet" type="text/css" href="style.css">
