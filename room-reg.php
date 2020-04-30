df29307c97798aa60926295a96b7bcb8504886e6
Mihir Soni <msonii2000@gmail.com>
2020-04-30 20:52:32 +0530

    update

--- room-reg.php
+++ room-reg.php
@@ -74,23 +74,23 @@
             <p style="display:inline-block;font-size: 25px;color:black;">Register Rooms</p>
         </div>
         
-        <form onsubmit="return validateF();" class="form" style="position: relative; text-align:left;margin-left:20px;display: inline-block;" method="post" action="submit-room.php" enctype="multipart/form-data" >
+        <form onsubmit="return validateF();" class="form" style="position: relative; text-align:left;margin-left:20px;display: inline-block; font-size: 13px;" method="post" action="submit-room.php" enctype="multipart/form-data" >
             
-            <div class="form-container" style="width: 350px;">
-                <input style="text-align: center; width: 300px;" id="b-name" type="" class="inputs" name="bn" placeholder="Service name" required="">
-                <input style="text-align: center; width: 300px;" id="h-no" type="" class="inputs" name="b" placeholder="Building/House no" required="">  
-                <select required="" style="height: 50px; width: 310px;text-align: center;" class="inputs" onchange="print_city('state', this.selectedIndex);" id="sts" name ="s" class="form-control" required></select>
+            <div class="form-container" style="width: 230px;">
+                <input style="text-align: center; width: 200px;" id="b-name" type="" class="inputs" name="bn" placeholder="Service name" required="">
+                <input style="text-align: center; width: 200px;" id="h-no" type="" class="inputs" name="b" placeholder="Building/House no" required="">  
+                <select required="" style="height: 50px; width: 210px;text-align: center;" class="inputs" onchange="print_city('state', this.selectedIndex);" id="sts" name ="s" class="form-control" required></select>
                 <script language="javascript">print_state("sts");</script>
-                <select required="" style="height: 50px;width: 310px; text-align: center;" class="inputs" id ="state" name="c" class="form-control" required></select>
+                <select required="" style="height: 50px;width: 210px; text-align: center;" class="inputs" id ="state" name="c" class="form-control" required></select>
                 <script type="text/javascript" src="area.js"></script>
-                <input required="" autocomplete="off" id="search_input" style="text-align: center; width: 300px;" type="" class="inputs" name="a" placeholder="Area/Coloney/Street" onkeyup="getin();" >
+                <input required="" autocomplete="off" id="search_input" style="text-align: center; width: 200px;" type="" class="inputs" name="a" placeholder="Area/Coloney/Street" onkeyup="getin();" >
                 <span id="search_res_list" class="overlays" style="position: absolute; left: 2px; top: 200px; width: 300px;">
                 </span>
-                <input id="Landmark" style="text-align: center; width: 300px;" type="" class="inputs" name="l" placeholder="Landmark" >
+                <input id="Landmark" style="text-align: center; width: 200px;" type="" class="inputs" name="l" placeholder="Landmark" >
             </div>   
 
-            <div class="form-container" style="width: 350px;">
-                <input required="" id="pc" style="text-align: center; width: 300px;" type="" class="inputs" name="p" placeholder="Pincode" >
+            <div class="form-container" style="width: 300px;">
+                <input required="" id="pc" style="text-align: center; width: 200px;" type="" class="inputs" name="p" placeholder="Pincode" >
                 <script type="text/javascript">
                             function pop(id)
                             {   var el= document.getElementById(id);
@@ -108,13 +108,13 @@
                             }
 
                 </script>
-                <button id="geo-l" onclick="pop('pop-up');" style="text-align: center; width: 310px; height: 50px; cursor: pointer;" type="button" class="inputs" name="loc">
+                <button id="geo-l" onclick="pop('pop-up');" style="text-align: center; width: 210px; height: 50px; cursor: pointer;" type="button" class="inputs" name="loc">
                                 Add geo-location
                 </button>
 
                 <input type="" id="lnglat" class="inputs" name="lnglat" style="display: none;">
                 <div style="display: inline-block; text-align: center;">
-                    <label class="inputs" for="photos" style="position:absolute;cursor:pointer;width:310px;height:25px;display:block; font-size: 14px; padding: 10px 0px 10px 0px;">Upload photos</label>
+                    <label class="inputs" for="photos" style="position:absolute;cursor:pointer;width:210px;height:25px;display:block; font-size: 14px; padding: 10px 0px 10px 0px;">Upload photos</label>
                     <input id="photos" class="inputs" style="opacity:0; z-index:-1;text-align: center;" type="file" name="uploadfile[]"  multiple>
                 </div>
                 <br>
@@ -130,8 +130,7 @@
 
                                 Anyone &nbsp<label for="ao" style="position: absolute; margin-top: 3px; margin-left: 3px;" ><div  onclick="select(this,'room-type-label');" style="width:10px; height: 10px; border-style: none; border-radius: 50%; border:solid 5px white; background-color: white;" name="room-type-label"></div></label>
                                 <input required="" type="radio" name="gender" id ="ao" value="Anyone">
-                                
-                                <hr style="border: solid .5px gray;">
+                                <br><br>
 
                                 Student &nbsp<label for="st" style="position: absolute; margin-top: 3px; margin-left: 3px;" ><div  onclick="select(this,'room-for-label');" style="width:10px; height: 10px; border-style: none; border-radius: 50%; border:solid 5px white; background-color: white;" name="room-for-label"></div></label>
                                 <input required="" type="radio" name="work" id="st" value="Student">
@@ -149,7 +148,7 @@
                 <br><br>
                 
             </div>  
-            <div class="form-container" style="width: 380px;" >
+            <div class="form-container" style="width: 280px;" >
                 <span style="font-weight: 600;">Facilities provided</span> <br><hr style="border:solid .5px; margin-right: 30px;">        
                             
                 <div style="display: inline-block; margin-right: 20px; margin-bottom: 10px;">
@@ -161,12 +160,11 @@
                             Parking Area &nbsp<label for="pa" style="position: absolute; margin-top: 2px;" ><div  onclick="check('pa-check');" style="width:20px; height: 20px; border-style: none; background-color: white;"><i id="pa-check" style="display:none; font-size: 20px;" class="fa fa-check" aria-hidden="true"></i></div></label>
                              <input type="checkbox" name="facilites[]" id="pa" value="Parking Area">
                 </div>
-
+                <br><br>
                 <div style="display: inline-block; margin-right: 20px;">
                             Bed &nbsp<label for="Bed" style="position: absolute; margin-top: 2px;" ><div  onclick="check('Bed-check');" style="width:20px; height: 20px; border-style: none; background-color: white;"><i id="Bed-check" style="display:none; font-size: 20px;" class="fa fa-check" aria-hidden="true"></i></div></label>
                             <input type="checkbox" name="facilites[]" id="Bed" value="Bed">
                 </div>
-                <br><br>
                 <div style="display: inline-block; margin-right: 20px;">
                             WiFi &nbsp<label for="wifi" style="position: absolute; margin-top: 2px;" ><div  onclick="check('wifi-check');" style="width:20px; height: 20px; border-style: none; background-color: white;"><i id="wifi-check" style="display:none; font-size: 20px;" class="fa fa-check" aria-hidden="true"></i></div></label>
                             <input type="checkbox" name="facilites[]" id="wifi" value="Wifi">
@@ -176,12 +174,11 @@
                             Cup Board &nbsp<label for="Cup Board" style="position: absolute; margin-top: 2px;" ><div  onclick="check('gsr-check');" style="width:20px; height: 20px; border-style: none; background-color: white;"><i id="gsr-check" style="display:none; font-size: 20px;" class="fa fa-check" aria-hidden="true"></i></div></label>
                             <input type="checkbox" name="facilites[]" id="Cup Board" value="Cup Board">
                 </div>
-
+                <br><br>
                 <div style="display: inline-block; margin-right: 20px;">
                             Room service &nbsp<label for="rs" style="position: absolute; margin-top: 2px;" ><div  onclick="check('rs-check');" style="width:20px; height: 20px; border-style: none; background-color: white;"><i id="rs-check" style="display:none; font-size: 20px;" class="fa fa-check" aria-hidden="true"></i></div></label>
                             <input type="checkbox" name="facilites[]" id="rs" value="Room Service">
-                </div>
-                <br><br>
+                </div><br><br>
                 <div style="display: inline-block; margin-right: 20px;">
                             Attached Bathrooms &nbsp<label for="ab" style="position: absolute; margin-top: 2px;" ><div  onclick="check('ab-check');" style="width:20px; height: 20px; border-style: none; background-color: white;"><i id="ab-check" style="display:none; font-size: 20px;" class="fa fa-check" aria-hidden="true"></i></div></label>
                             <input type="checkbox" name="facilites[]" id="ab" value="Attach Bathrooms">
@@ -199,7 +196,7 @@
                             2-Room &nbsp<label for="2r" style="position: absolute; margin-top: 2px;" onclick="disp('r-2');"><div  onclick="check('2r-check');" style="width:20px; height: 20px; border-style: none; background-color: white;"><i id="2r-check" style="display:none; font-size: 20px;" class="fa fa-check" aria-hidden="true"></i></div></label>
                             <input type="checkbox" name="room[]" id="2r" value="2r" >
                 </div>
-
+                <br><br>
                 <div style="display: inline-block; margin-right: 20px;">
                             Room & Kitchen &nbsp<label for="rk" style="position: absolute; margin-top: 2px;" onclick="disp('rk-t');"><div  onclick="check('rk-check');" style="width:20px; height: 20px; border-style: none; background-color: white;"><i id="rk-check" style="display:none; font-size: 20px;" class="fa fa-check" aria-hidden="true"></i></div></label>
                             <input type="checkbox" name="room[]" id="rk" value="rk" >
@@ -220,23 +217,20 @@
                 
             </div>
 
-            <div class="form-container" style="width: 300px;">
-                Rules <hr style="border:solid .5px;"><textarea style="width: 200px; border: none; height: 100px; font-family: segoe UI; font-size: 13px;" placeholder="Write here"></textarea>               
+            <div class="form-container" style="width: 350px; height: 200px;">
+                <b>Rules </b> <hr style="border:solid .5px;"><textarea style="width: 200px; border: none; height: 100px; font-family: segoe UI; font-size: 13px;" placeholder="Write here"></textarea>               
             </div>
-                    
-            <br><br>                
-            </div>
+                  
             <br><br>
 
-            <div style="height:70px;display: inline-block; margin-bottom: 50px;">
+            <div style="height:50px; position: relative; float: left; margin-bottom: 50px;">
             I accept <a href="" style="text-decoration: underline; color:blue;">Terms & conditions</a>
-
-            <div style="display: inline-block; margin-right: 20px; margin-bottom: 17px;">
+            <div style="display: inline-block;  margin-bottom: 17px;">
                             <label for="tnc" style="position: absolute; margin-top: 2px;" ><div  onclick="check('tnc-check');" style="width:20px; height: 20px; border-style: none; background-color: white;"><i id="tnc-check" style="display:none; font-size: 20px;" class="fa fa-check" aria-hidden="true"></i></div></label>
                             <input style="display:none;" type="radio" name="tnc" id="tnc" value="tnc">
             </div>
             <br><br>
-            <input style="position:inherit;width: 300px;"  type="submit" value="Submit" class="btn" name="submit" onclick="">
+            <input style="position:relative;width:200px;"  type="submit" value="Submit" class="btn" name="submit" onclick="">
             </div>
             
 
