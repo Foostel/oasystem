9d6eb07ddbbf9c93ecc148db1dfae73815fd5a52
Mihir Soni <msonii2000@gmail.com>
2020-04-30 20:55:39 +0530

    Update db-connection.php

--- db-connection.php
+++ db-connection.php
@@ -8,8 +8,8 @@
          $dbuser = 'root';
          $dbpass = '';
          $db='foostel';
+*/
          $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
-  */       
          if(! $conn ) {
             die('Could not connect: ' . mysql_error());
          }
